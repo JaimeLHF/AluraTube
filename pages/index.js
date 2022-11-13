d19c2f6deee2d99f2c00d5765d66631e5d36d428
JaimeLHF <jaimehansenfilho@gmail.com>
2022-11-13 18:07:00 -0300

    Atalização Aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,75 @@
-import config from "../config.json"
-import styled from "styled-components"
+import React from "react";
+import config from "../config.json";
+import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
-import Menu from "../src/components/Menu"
-import {StyledTimeline} from "../src/components/Timeline"
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-  console.log(config.playlists);
+  const estilosDaHomePage = {
+    // backgroundColor: "red" 
+  };
+
+  const [valorDoFiltro, setValorDoFiltro] = React.useState("");
+  
+
   return (
-  <>
-  <CSSReset />
-    <div>
-      <Menu />
-      <Header />
-      <TimeLine playlists={config.playlists} />
-    </div>
+    <>
+      <CSSReset />
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        // backgroundColor: "red",
+      }}>
+        <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro}/>
+        <Header />
+        <Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
+          Conteúdo
+        </Timeline>
+      </div>
     </>
-  )
+  );
 }
 
 export default HomePage
 
+// function Menu() {
+//     return (
+//         <div>
+//             Menu
+//         </div>
+//     )
+// }
 
 
 const StyledHeader = styled.div`
-  img {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-  }  
-  .user-info{
-    margin-top: 50px;
-    display: flex;
-    align-items: center;
-    width: 100%;
-    padding: 16px 32px;
-    gap: 16px;
-  }
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info {        
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
 `;
 
+const StyledBanner = styled.div`
+  background-color: blue;
+  height: 230px;
+  background-image: url(${({bg}) => bg});
+`;
 
 function Header() {
   return (
     <StyledHeader>
+      <StyledBanner bg={config.bg}/>
       {/* <img src="banner" /> */}
       <section className="user-info">
-
         <img src={`https://github.com/${config.github}`} />
         <div>
           <h2>
@@ -54,28 +79,33 @@ function Header() {
             {config.job}
           </p>
         </div>
-
       </section>
     </StyledHeader>
   )
 }
 
-function TimeLine(props) {
-  console.log("Dentro do componente", props.playlists);
-  const playlistNames = Object.keys(props.playlists);
-
+function Timeline({searchValue, ...propriedades}) {
+  // console.log("Dentro do componente", propriedades.playlists);
+  const playlistNames = Object.keys(propriedades.playlists);
+  // Statement
+  // Retorno por expressão
   return (
     <StyledTimeline>
       {playlistNames.map((playlistName) => {
-        const videos = props.playlists[playlistName];
-        console.log(videos)
+        const videos = propriedades.playlists[playlistName];
+        // console.log(playlistName);
+        // console.log(videos);
         return (
-          <section>
+          <section key={playlistName}>asd
             <h2>{playlistName}</h2>
             <div>
-              {videos.map((video) => {
+              {videos.filter((video) => {
+                const titleNormalize = video.title.toLowerCase();
+                const searchValueNormalize = searchValue.toLowerCase();
+                return titleNormalize.includes(searchValueNormalize)
+              }).map((video) => {
                 return (
-                  <a href={video.url}>
+                  <a key={video.url} href={video.url}>
                     <img src={video.thumb} />
                     <span>
                       {video.title}
@@ -86,9 +116,7 @@ function TimeLine(props) {
             </div>
           </section>
         )
-      }
-
-      )}
+      })}
     </StyledTimeline>
   )
 }
